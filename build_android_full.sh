ecca9dd1bfc2b0a82e0812f89da39a0d768749c0
zhufu <231043095@qq.com>
2017-06-21 10:49:48 +0800

    add build_android_full.sh

---
+++ build_android_full.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+export TMPDIR=/Users/zhufu/ffmpeg-3.0/ffmpegtemp #这句很重要，不然会报错 unable to create temporary file in
+
+# NDK的路径，根据自己的安装位置进行设置
+NDK=/Users/zhufu/android-ndk-r12b
+
+# 编译针对的平台，可以根据自己的需求进行设置
+# 这里选择最低支持android-14, arm架构，生成的so库是放在
+# libs/armeabi文件夹下的，若针对x86架构，要选择arch-x86
+PLATFORM=$NDK/platforms/android-14/arch-arm
+
+# 工具链的路径，根据编译的平台不同而不同
+# arm-linux-androideabi-4.9与上面设置的PLATFORM对应，4.9为工具的版本号，
+# 根据自己安装的NDK版本来确定，一般使用最新的版本
+TOOLCHAIN=$NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/darwin-x86_64
+
+function build_one
+{
+./configure \
+    --prefix=$PREFIX \
+    --enable-shared \
+    --disable-static \
+    --disable-doc \
+    --disable-ffmpeg \
+    --disable-ffplay \
+    --disable-ffprobe \
+    --disable-ffserver \
+    --disable-avdevice \
+    --disable-doc \
+    --disable-symver \
+    --cross-prefix=$TOOLCHAIN/bin/arm-linux-androideabi- \
+    --target-os=linux \
+    --arch=arm \
+    --enable-cross-compile \
+    --sysroot=$PLATFORM \
+    --extra-cflags="-I$PLATFORM/usr/include" \
+#    --extra-ldflags="-L$PLATFORM/usr/lib -nostdlib" \ #①这一句我注释掉了，不然会报 ld: -single_module: unknown option
+
+$ADDITIONAL_CONFIGURE_FLAG
+sed -i '' 's/HAVE_LRINT 0/HAVE_LRINT 1/g' config.h
+sed -i '' 's/HAVE_LRINTF 0/HAVE_LRINTF 1/g' config.h
+sed -i '' 's/HAVE_ROUND 0/HAVE_ROUND 1/g' config.h
+sed -i '' 's/HAVE_ROUNDF 0/HAVE_ROUNDF 1/g' config.h
+sed -i '' 's/HAVE_TRUNC 0/HAVE_TRUNC 1/g' config.h
+sed -i '' 's/HAVE_TRUNCF 0/HAVE_TRUNCF 1/g' config.h
+sed -i '' 's/HAVE_CBRT 0/HAVE_CBRT 1/g' config.h
+sed -i '' 's/HAVE_RINT 0/HAVE_RINT 1/g' config.h
+
+make clean
+make -j4
+make install
+
+}
+
+# arm v7vfp
+CPU=armv7-a
+OPTIMIZE_CFLAGS="-mfloat-abi=softfp -mfpu=vfp -marm -march=$CPU "
+PREFIX=./android/$CPU-vfp
+ADDITIONAL_CONFIGURE_FLAG=
+build_one
+
+# CPU=armv
+# PREFIX=$(pwd)/android/$CPU
+# ADDI_CFLAGS="-marm"
+# build_one
+
+#arm v6
+#CPU=armv6
+#OPTIMIZE_CFLAGS="-marm -march=$CPU"
+#PREFIX=./android/$CPU 
+#ADDITIONAL_CONFIGURE_FLAG=
+#build_one
+
+#arm v7vfpv3
+# CPU=armv7-a
+# OPTIMIZE_CFLAGS="-mfloat-abi=softfp -mfpu=vfpv3-d16 -marm -march=$CPU "
+# PREFIX=./android/$CPU
+# ADDITIONAL_CONFIGURE_FLAG=
+# build_one
+
+#arm v7n
+#CPU=armv7-a
+#OPTIMIZE_CFLAGS="-mfloat-abi=softfp -mfpu=neon -marm -march=$CPU -mtune=cortex-a8"
+#PREFIX=./android/$CPU 
+#ADDITIONAL_CONFIGURE_FLAG=--enable-neon
+#build_one
+
+#arm v6+vfp
+#CPU=armv6
+#OPTIMIZE_CFLAGS="-DCMP_HAVE_VFP -mfloat-abi=softfp -mfpu=vfp -marm -march=$CPU"
+#PREFIX=./android/${CPU}_vfp 
+#ADDITIONAL_CONFIGURE_FLAG=
+#build_one
